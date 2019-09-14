580eb9ba0a49af93cd574da9d6bcfe2d97533773
OsukeUesugi <funxion.mail@gmail.com>
2019-09-14 15:05:55 +0900

    added id property to JSON file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 
+interface Item {
+  id: string;
+  area: string;
+  name: string;
+  url: string;
+}
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://www.bs-tbs.co.jp/sakaba/map/index.html');
   const item = await page.$$('.kihon1, .kihon5')
-  const data = { items:[] };
+  const data: { items: Item[] } = { items:[] };
 
   let area = '';
 
@@ -16,18 +22,20 @@ const fs = require('fs');
     if (thisClass === 'kihon1') {
       area = await (await item[i].getProperty('textContent')).jsonValue()
     } else {
-      const info = {
+      const url: string = await (await item[i].getProperty('href')).jsonValue();
+      const urlArr = url.split('/');
+      const id = urlArr[urlArr.length - 1].split('.html')[0];
+      const info: Item = {
+        id,
         area,
         name: await (await item[i].getProperty('textContent')).jsonValue(),
-        url: await (await item[i].getProperty('href')).jsonValue()
+        url: url,
       };
       data.items.push(info);
-      //console.log(await (await item[i].getProperty('textContent')).jsonValue())
-      //console.log(await (await item[i].getProperty('href')).jsonValue());
     }
   }
 
-  fs.writeFile(`dist/res.json`, JSON.stringify(data), function (err) {
+  fs.writeFile(`dist/res.json`, JSON.stringify(data), function (err: Error) {
     console.log(err);
   });
 
